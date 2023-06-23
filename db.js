29fb79830ab22b310c1aef36b357498a6888e70e
Devansh Pareek <devanshpareek863@gmail.com>
2023-06-23 13:08:47 +0530

    room list retrieved and showed in HomeScreen

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { connect } = require('http2');
 const mongoose = require('mongoose');
-var mongoUrl = 'mongodb+srv://test_user:test-pass@cluster0.uoiobel.mongodb.net/mern-hotels';
+var mongoUrl = 'mongodb+srv://test_user:test-pass@cluster0.uoiobel.mongodb.net/mern-rooms?retryWrites=true&w=majority';
 
-mongoose.connect(mongoUrl , {useUnifiedTopology: true , useNewUrlParser : true });
+mongoose.connect(mongoUrl, { useUnifiedTopology: true, useNewUrlParser: true });
 var connection = mongoose.connection
 
-connection.on('error' , ()=>
+connection.on('error', () =>
     console.log('db Connection faild')
 )
 connection.on('connected', () =>
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 const dbConfig = require('./db');
-const port = process.env.port || 5501;
+const roomsRoute = require('./routes/roomsRoute');
 
+const port = process.env.port || 5000;
+
+app.use('/api/rooms', roomsRoute);
 app.listen(port, () => console.log('Server started successfully on port : ${port} using nodemon'));

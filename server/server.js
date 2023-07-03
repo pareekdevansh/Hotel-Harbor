4ef3913d28fc15a138e45e088c29f29716abf452
Devansh Pareek <devanshpareek863@gmail.com>
2023-07-04 00:51:15 +0530

    project strucutre updated & Bundler Polyfill Issues resolved

---
+++ server/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dbConfig = require("./db");
+require("dotenv").config();
+const authRoute = require("./routes/authRoute");
+const roomsRoute = require("./routes/roomsRoute");
+const bookingsRoute = require("./routes/bookingsRoute");
+const stripeRoute = require("./routes/stripeRoute");
+const privateRoute = require("./routes/privateRoute");
+const usersRoute = require("./routes/usersRoute");
+const adminRoute = require("./routes/adminRoute");
+const errorHandler = require("./middleware/error");
+const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    headers: {
+      "Access-Control-Allow-Origin": process.env.CLIENT_URL,
+    }
+  })
+);
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res, next) => {
+  res.send(`Api running`);
+});
+
+//routes
+app.use("/api/private", privateRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/rooms", roomsRoute);
+app.use("/api/bookings", bookingsRoute);
+app.use("/api/stripe", stripeRoute);
+app.use("/api/admin", adminRoute);
+app.use(errorHandler); // error handler : should be last in the middlewares
+const server = app.listen(port, () =>
+  console.log(
+    `server started on port : ${port} with url ${process.env.SERVER_URL}`
+  )
+);
+process.on("unhandledRejection", (error, promise) => {
+  console.log(`Logged Error: ${error}`);
+  server.close(() => process.exit(1));
+});
